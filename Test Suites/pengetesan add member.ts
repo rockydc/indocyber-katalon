63890e1489cda1d5a80ec2e3384f7b839595c12a
rockydc <rockydaniel182@gmail.com>
2021-10-01 09:05:02 +0700

    add test suite collection

--- Test Suites/pengetesan add member.ts
+++ Test Suites/pengetesan add member.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Scenario_indocyber</name>
+   <name>pengetesan add member</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -11,6 +11,24 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>2706b993-6f4c-46e5-a5a0-f9fcfa6b452a</testSuiteGuid>
+   <testCaseLink>
+      <guid>7be81adf-6cab-41ee-baf2-58a618cba9a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add Data Member</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ae2324f1-b336-4126-8f14-730401659cad</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>4855cd97-56b1-4720-b832-9f4f5f5e9814</variableId>
+      </variableLink>
+   </testCaseLink>
    <testCaseLink>
       <guid>4daccd35-96b3-41fc-bdbd-936cc9cc44fb</guid>
       <isReuseDriver>false</isReuseDriver>
